be619faabeb0421a8a42df8830ae8c9e737a6fde
serge <kislak7@gmail.com>
2022-01-08 01:38:19 +0300

    initial app

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const {
+  PORT = 3000,
+} = process.env;
+
+const express = require('express');
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log('server is up and running');
+});
