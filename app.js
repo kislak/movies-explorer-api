492da70aa962022b47fc2cccf881fd4be29287fb
serge <kislak7@gmail.com>
2022-01-08 21:31:49 +0300

    Add cors options

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const {
   PORT = 3000,
+  CORS_ORIGIN = '',
   MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb',
+  NODE_ENV = 'development',
 } = process.env;
 
 const express = require('express');
@@ -22,12 +24,28 @@ const app = express();
 
 mongoose.connect(MONGO_URL, { useNewUrlParser: true });
 
+if (CORS_ORIGIN) {
+  const cors = require('cors');
+  const whitelist = CORS_ORIGIN.split(' ');
+  const corsOptions = {
+    origin(origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
+}
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
 app.get('/', (req, res) => res.send('Ответ на сигнал из далёкого космоса'));
-
 app.use('/', authRouter);
 app.use(auth);
 app.use('/users', usersRouter);
